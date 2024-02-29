ec3376741fe5e9c2d24ad2e2d337e3a9f46e7462
James Phelps <james.elias.phelps@gmail.com>
2024-02-29 10:30:06 -0700

    PowerOn, Standby, SwitchInput, SetVolume, GetPower

---
+++ device/device.go
@@ -0,0 +1,51 @@
+package device
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type DeviceManager struct {
+	Log *zap.Logger
+}
+
+func (d *DeviceManager) RunHTTPServer(router *gin.Engine, port string) error {
+	d.Log.Info("registering http endpoints")
+	// action endpoints
+	route := router.Group("")
+	route.GET("/:address/power/on", d.PowerOn)
+	route.GET("/:address/power/standby", d.Standby)
+	route.GET("/:address/input/:port", d.SwitchInput)
+	route.GET("/:address/volume/set/:value", d.SetVolume)
+	// route.GET("/:address/volume/mute", d.VolumeMute)
+	// route.GET("/:address/volume/unmute", d.VolumeUnmute)
+	// route.GET("/:address/display/blank", d.BlankDisplay)
+	// route.GET("/:address/display/unblank", d.UnblankDisplay)
+
+	// status endpoints
+	route.GET("/:address/power/status", d.GetPower)
+	// route.GET("/:address/input/current", d.GetInput)
+	// route.GET("/:address/input/list", d.GetInputList)
+	// route.GET("/:address/active/:port", d.GetActiveSignal)
+	// route.GET("/:address/volume/level", d.GetVolume)
+	// route.GET("/:address/volume/mute/status", d.GetMute)
+	// route.GET("/:address/display/status", d.GetBlank)
+	// route.GET("/:address/hardware", d.GetHardwareInfo)
+
+	// log level endpoints
+	// router.PUT("/log-level/:level", log.SetLogLevel)
+	// router.GET("/log-level", log.GetLogLevel)
+
+	server := &http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1021 * 10,
+	}
+
+	d.Log.Info("running http server")
+	router.Run(server.Addr)
+
+	return fmt.Errorf("http server stopped")
+}
--- device/rpc.go
+++ device/rpc.go
@@ -1,4 +1,4 @@
-package handlers
+package device
 
 import (
 	"fmt"
@@ -7,52 +7,59 @@ import (
 	"strings"
 	"time"
 
-	"github.com/byuoitav/sony-control-microservice/helpers"
+	"github.com/byuoitav/sony-control-microservice/device/helpers"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/common/status"
 	"github.com/labstack/echo"
 )
 
-func PowerOn(context echo.Context) error {
-	log.L.Infof("Powering on %s...", context.Param("address"))
+func (d *DeviceManager) PowerOn(context *gin.Context) {
+	d.Log.Debug("Powering on %s...", zap.String("address", context.Param("address")))
 
-	err := helpers.SetPower(context.Request().Context(), context.Param("address"), true)
+	err := helpers.SetPower(context, context.Param("address"), true)
 	if err != nil {
-		log.L.Debugf("Error: %v", err.Error())
-		return context.JSON(http.StatusInternalServerError, err.Error())
+		d.Log.Warn("could not get power", zap.Error(err))
+		context.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	log.L.Debugf("Done.")
-	return context.JSON(http.StatusOK, status.Power{Power: "on"})
+	d.Log.Debug("Powered on", zap.String("power", "on"))
+	context.JSON(http.StatusOK, status.Power{Power: "on"})
 }
 
-func Standby(context echo.Context) error {
-	log.L.Infof("Powering off %s...", context.Param("address"))
+func (d *DeviceManager) Standby(context *gin.Context) {
+	d.Log.Debug("Powering off %s...", zap.String("address", context.Param("address")))
 
-	err := helpers.SetPower(context.Request().Context(), context.Param("address"), false)
+	err := helpers.SetPower(context, context.Param("address"), false)
 	if err != nil {
-		log.L.Debugf("Error: %v", err.Error())
-		return context.JSON(http.StatusInternalServerError, err.Error())
+		d.Log.Warn("could not power off", zap.Error(err))
+		context.JSON(http.StatusInternalServerError, err.Error())
+		return
 	}
 
-	log.L.Infof("Done.")
-	return context.JSON(http.StatusOK, status.Power{Power: "standby"})
+	d.Log.Debug("Powered off", zap.String("power", "off"))
+	context.JSON(http.StatusOK, status.Power{Power: "standby"})
 }
 
-func GetPower(context echo.Context) error {
-	log.L.Infof("Getting power status of %s...", context.Param("address"))
+func (d *DeviceManager) GetPower(context *gin.Context) {
+	d.Log.Debug("Getting power status of %s...", zap.String("address", context.Param("address")))
 
-	response, err := helpers.GetPower(context.Request().Context(), context.Param("address"))
+	response, err := helpers.GetPower(context, context.Param("address"))
 	if err != nil {
-		return context.JSONBlob(http.StatusInternalServerError, []byte(err.Error()))
+		d.Log.Warn("Failed to get Power Status", zap.Error(err))
+		context.JSON(http.StatusInternalServerError, []byte(err.Error()))
+		return
 	}
 
-	return context.JSON(http.StatusOK, response)
+	d.Log.Debug("Getting Status", zap.String("Status", response.Power))
+	context.JSON(http.StatusOK, response)
 }
 
-func SwitchInput(context echo.Context) error {
-	log.L.Infof("Switching input for %s to %s ...", context.Param("address"), context.Param("port"))
+func (d *DeviceManager) SwitchInput(context *gin.Context) {
+	d.Log.Debug("Switching input for %s to %s ...", zap.String("address", context.Param("address")), zap.String("port", context.Param("port")))
 	address := context.Param("address")
 	port := context.Param("port")
 
@@ -60,31 +67,32 @@ func SwitchInput(context echo.Context) error {
 
 	params := make(map[string]interface{})
 	if len(splitPort) < 2 {
-		return context.JSON(http.StatusBadRequest, fmt.Sprintf("ports configured incorrectly (should follow format \"hdmi!2\"): %s", port))
+		context.JSON(http.StatusBadRequest, fmt.Sprintf("ports configured incorrectly (should follow format \"hdmi!2\"): %s", port))
 	}
 	params["uri"] = fmt.Sprintf("extInput:%s?port=%s", splitPort[0], splitPort[1])
 
 	err := helpers.BuildAndSendPayload(address, "avContent", "setPlayContent", params)
 	if err != nil {
-		return context.JSON(http.StatusInternalServerError, err.Error())
+		context.JSON(http.StatusInternalServerError, err.Error())
 	}
 
 	log.L.Debugf("Done.")
-	return context.JSON(http.StatusOK, status.Input{Input: port})
+	context.JSON(http.StatusOK, status.Input{Input: port})
 }
 
-func SetVolume(context echo.Context) error {
+func (d *DeviceManager) SetVolume(context *gin.Context) {
 	address := context.Param("address")
 	value := context.Param("value")
 
 	volume, err := strconv.Atoi(value)
 	if err != nil {
-		return context.JSON(http.StatusBadRequest, err.Error())
+		context.JSON(http.StatusBadRequest, err.Error())
 	} else if volume > 100 || volume < 0 {
-		return context.JSON(http.StatusBadRequest, "Error: volume must be a value from 0 to 100!")
+		context.JSON(http.StatusBadRequest, "Error: volume must be a value from 0 to 100!")
 	}
 
 	log.L.Debugf("Setting volume for %s to %v...", address, value)
+	d.Log.Debug("Setting volume for %s to %v...", zap.String("address", context.Param("address")), zap.String("value", context.Param("value")))
 
 	params := make(map[string]interface{})
 	params["target"] = "speaker"
@@ -92,7 +100,7 @@ func SetVolume(context echo.Context) error {
 
 	err = helpers.BuildAndSendPayload(address, "audio", "setAudioVolume", params)
 	if err != nil {
-		return context.JSON(http.StatusInternalServerError, err.Error())
+		context.JSON(http.StatusInternalServerError, err.Error())
 	}
 
 	//do the same for the headphone
@@ -102,11 +110,11 @@ func SetVolume(context echo.Context) error {
 
 	err = helpers.BuildAndSendPayload(address, "audio", "setAudioVolume", params)
 	if err != nil {
-		return context.JSON(http.StatusInternalServerError, err.Error())
+		context.JSON(http.StatusInternalServerError, err.Error())
 	}
 
 	log.L.Debugf("Done.")
-	return context.JSON(http.StatusOK, status.Volume{Volume: volume})
+	context.JSON(http.StatusOK, status.Volume{Volume: volume})
 }
 
 func VolumeUnmute(context echo.Context) error {
